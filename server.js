c5011eb389075f5f5a18c7fb0752c15660691881
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-07 17:41:52 -0500

    Refactored a few lines

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const config = require('./config.js');
-const bodyParser = require('body-parser');
 
 const server = express();
-
-server.use(bodyParser.json());
-
 const PORT = config.port;
 
 const detailsArray = [];
 
-
 server.get('/place', (req, res) => {
   const textSearch = req.query.textSearch;
-  fetch(
-    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${textSearch}&key=${
-      config.gmaps.apiKey
-    }`
-  )
+  let placeEndPoint = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${textSearch}&key=${config.gmaps.apiKey}`
+  fetch(placeEndPoint)
     .then(res => res.json())
     .then(json => {
       let placeIdArr = json.results.map(element => element.place_id);
       placeIdArr.forEach(place => {
-        fetch(
-          `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place}&key=${
-            config.gmaps.apiKey
-          }`
-        )
+        let detailEndPoint = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place}&key=${config.gmaps.apiKey}`
+        fetch(detailEndPoint)
           .then(res => res.json())
           .then(json => detailsArray.push(json));
       });
